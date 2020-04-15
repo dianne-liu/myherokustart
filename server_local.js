19fa130c6626faae589ea68141c5c2023c2184ec
dianne-liu <53639951+dianne-liu@users.noreply.github.com>
2020-04-15 11:22:18 +0800

    Add files via upload

--- server_local.js
+++ server_local.js
@@ -2,10 +2,9 @@
 const http = require('http');
 
 //設定server網址，因為在本機端測試，所以輸入127.0.0.1
-//const hostname = '127.0.0.1'  //上傳至伺服器需拿掉
-
-//port 號會由 Heroku 給予，因此不再自行指定
-const port = process.env.PORT || 3000;
+const hostname = '127.0.0.1';
+//設定port號，這邊多少都可以，不要和現有的port重複就好
+const port = 3000;
 
 //新增一個server並指定他的頁面資訊，內容為'Hello World!'
 const server = http.createServer((req, res) => {
@@ -14,5 +13,7 @@ const server = http.createServer((req, res) => {
     res.end('Hello World!\n');
 });
 
-//監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Ready. Listening on ${port}`));
+//監聽剛剛新建的server，開啟後就執行裡面的console.log
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
